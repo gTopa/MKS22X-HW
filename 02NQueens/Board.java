5d601f60e82cc83552f4748ae3e6b9b68a700c90
gTopa <gtopa@stuy.edu>
2016-02-09 18:07:09 -0500

    created Board.java(except for func for printing, compiles

---
+++ 02NQueens/Board.java
@@ -0,0 +1,47 @@
+public class Board{
+    private int[][] board;
+
+    public Board(int n){
+	board=new int[n][n];
+    }
+
+    public boolean addQueen(int row, int col){
+	if(board[row][col]==0){
+	    board[row][col]=1;
+	    fixBoard(-1,row,col);
+	    return true;
+	}else{
+	    return false;
+	}
+    }
+
+    public boolean removeQueen(int row,int col){
+	if (board[row][col]==1){
+	    board[row][col]=0;
+	    fixBoard(1,row,col);
+	    return true;
+	}else{
+	    return false;
+	}
+    }
+    
+    public void fixBoard(int change, int row, int col){
+	if (change>0){
+	    for (int i=0;i<board.length*board.length;i++)
+		if (board[i/board.length][i%board.length]<0){
+		    board[1/board.length][i%board.length]+=1;
+		}
+	}else{
+	    for (int i=1;i<board.length-col;i++){
+		board[row][col+i]-=1;
+	    }
+	    for (int i=1;i<board.length-col&&i<board.length-row;i++){
+		board[row+i][col+i]-=1;
+	    }
+	    for (int i=1;i<board.length-col&&i<row+1;i++){
+		board[row-i][col+i]-=1;
+	    }
+	}
+    }
+}
+	
