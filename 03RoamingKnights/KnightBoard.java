461b2c6b3739c4668368206e945308619a271559
gTopa <gtopa@stuy.edu>
2016-02-12 13:15:34 -0500

    compiles, doesn't work

--- 03RoamingKnights/KnightBoard.java
+++ 03RoamingKnights/KnightBoard.java
@@ -1,29 +1,75 @@
 public class KnightBoard{
     private int[][] board;
 
-    public KnightBoard(int n){
-	board = new int[n+4][n+4];
-	for (int i=0;i<(n+4)*(n+4);i++){
-	    if (i/(n+4)==0||i/(n+4)==n+3||i%(n+4)==0||i%(n+4)==n+3||i/(n+4)==1||i/(n+4)==n+2||i%(n+4)==1||i%(n+4)==n+2){
-		board[i/(n+4)][i%(n+4)]=-1;
+    public KnightBoard(int col, int row){
+	board = new int[row+4][col+4];
+	for (int i=0;i<(row+4)*(col+4);i++){
+	    if (i/(row+4)==0||i/(row+4)==row+3||i%(col+4)==0||i%(col+4)==col+3||i/(row+4)==1||i/(row+4)==row+2||i%(col+4)==1||i%(col+4)==col+2){
+		board[i/(row+4)][i%(col+4)]=-1;
 	    }
 	}
 
     }
 
     public boolean solve(){
-	solveH(board.length/2,board.length/2,1);
+	return solveH(board.length/2,board[0].length/2,1);
     }
 
-    public boolean solveH(int row, int col, int turn){
+    public boolean solveH(int col, int row, int turn){
 	if (turn==(board.length-4)*(board.length-4)+1&&board[row][col]==1){
 	    return true;
+	}else{
+	    for (int i=0;i<8;i++){
+		if (i==0){
+		    if (moveKnight(turn,row+2,col+1)){
+			return solveH(col+1,row+2,turn+1);
+		    }
+		}else if (i==1){
+		    if (moveKnight(turn,row+2,col-1)){
+			return solveH(col-1,row+2,turn+1);
+		    }
+		}else if (i==2){
+		    if (moveKnight(turn,row-2,col+1)){
+			return solveH(col+1,row-2,turn+1);
+		    }
+		}else if (i==3){
+		    if (moveKnight(turn,row-2,col-1)){
+			return solveH(col-1,row-2,turn+1);
+		    }
+		}else if (i==4){
+		    if (moveKnight(turn,row+1,col+2)){
+			return solveH(col+2,row+1,turn+1);
+		    }
+		}else if (i==5){
+		    if (moveKnight(turn,row+1,col-2)){
+			return solveH(col-2,row+1,turn+1);
+		    }
+		}else if (i==6){
+		    if (moveKnight(turn,row-1,col-2)){
+			return solveH(col-2,row-1,turn+1);
+		    }
+		}else{
+		    if (moveKnight(turn,row-1,col+2)){
+			return solveH(col+2,row-1,turn+1);
+		    }
+		}
+	    }
+	    return false;
 	}
     }
 
-    public boolean printSolution(){
+    public void printSolution(){
+	int rowL=board.length;
+	int colL=board[0].length;
+	for (int row=0;row<rowL;row++){
+	    for(int col=0;col<colL;col++){
+		if (!(row/(rowL)==0||row/(rowL)==rowL-3||col%(colL)==0||col%(colL)==colL-3||row/(rowL)==1||row/(rowL)==rowL-2||col%(colL)==1||col%(colL)==colL-2)){
+		    System.out.println(board[row/rowL][col%colL]);
+		}
+	    }
+	}
     }
-
+    
     public boolean moveKnight(int turn, int row, int col){
 	if (board[row][col]==0){
 	    board[row][col]=turn;
@@ -32,3 +78,20 @@ public class KnightBoard{
 	    return false;
 	}
     }
+
+    public void printBoard(){
+	for (int i=0;i<board.length;i++){
+	    for (int c=0;c<board[0].length;c++){
+		System.out.print(board[i][c]+" ");
+	    }
+	    System.out.println();
+	}
+    }
+
+    public static void main(String[]args){
+	KnightBoard b1=new KnightBoard(5,5);
+	b1.printBoard();
+	System.out.println(b1.solve());
+	b1.printSolution();
+    }
+}
