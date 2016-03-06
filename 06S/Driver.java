b1ed653119e1e0f3bccbcc4d5fbd2f8a24811445
gTopa <gtopa@stuy.edu>
2016-03-06 17:47:12 -0500

    done

--- 06S/Driver.java
+++ 06S/Driver.java
@@ -1,6 +1,6 @@
 public class Driver{
     public static void main(String[]args){
-	int[] a={2,1,4,3};
+	int[] a={2,0,1,4,3,2,9};
 	MergeSort.mergeSort(a);
 	MergeSort.printArray(a);
     }
--- 06S/MergeSort.java
+++ 06S/MergeSort.java
@@ -33,20 +33,32 @@ public class MergeSort{
     }
 
     public static void mergeSort(int[] data){
-	data=mergeSortH(data,0,data.length/2,data.length/2,data.length);
+	data=mergeSortH(data,0,data.length/2,data.length/2+1,data.length-1);
     }
 
     public static int[] mergeSortH(int[] data, int startA, int endA, int startB, int endB){
 	if (startA==endA&&startB==endB){
 	    return merge(data,startA,endA,startB,endB);
-	}else{
-	    int[] data1=mergeSortH(data,startA,endA/2,endA/2+1,endA);
-	    int[] data2=mergeSortH(data,startB,endB/2,endB/2+1,endB);
-	    for (int i=0;i<data1.length;i++){
+	}else if(startA==endA){
+	    int[] data1=mergeSortH(data,startB,(endB-startB)/2+startB,(endB-startB)/2+startB+1,endB);
+	    for(int i=startB;i<=endB;i++){
 		data[i]=data1[i];
 	    }
-	    for (int i=0;i<data2.length;i++){
-		data[i+startB]=data1[i];
+	    return merge(data,startA,endA,startB,endB);
+	}else if(startB==endB){
+	    int[] data2=mergeSortH(data,startA,(endA-startA)/2+startA,(endA-startA)/2+startA+1,endA);
+	    for(int i=startA;i<=endA;i++){
+		data[i]=data2[i];
+	    }
+	    return merge(data,startA,endA,startB,endB);
+	}else{
+	    int[] data1=mergeSortH(data,startA,(endA-startA)/2+startA,(endA-startA)/2+startA+1,endA);
+	    int[] data2=mergeSortH(data,startB,(endB-startB)/2+startB,(endB-startB)/2+startB+1,endB);
+	    for (int i=startA;i<=endA;i++){
+		data[i]=data1[i];
+	    }
+	    for (int j=startB;j<=endB;j++){
+		data[j]=data2[j];
 	    }
 	    return merge(data,startA,endA,startB,endB);
 	}
