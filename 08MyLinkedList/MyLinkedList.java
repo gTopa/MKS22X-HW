27927582f7b688292f050765bf997068ff414d42
gTopa <gtopa@stuy.edu>
2016-03-19 13:31:34 -0400

    finished my linked list

--- 08MyLinkedList/MyLinkedList.java
+++ 08MyLinkedList/MyLinkedList.java
@@ -21,23 +21,104 @@ public class MyLinkedList{
     }
     private int size;
     private LNode start;
-
+    private LNode end;
+    
     public boolean add(int value){
 	if (start==null){
 	    start=new LNode(value);
+	    size++;
+	    end=start;
 	    return true;
-	}
-	else{
-	    LNode current = start;
-	    while (current.getNext()!=null){
-		current=current.getNext();
-	    }
-	    current.setNext(new LNode(value));
+	}else{
+	    end.setNext(new LNode(value));
+	    end=end.getNext();
 	    size++;
 	    return true;
 	}
     }
+    
+    public boolean add(int value, int pos){
+	if(pos>size){
+	    return false;
+	}else if(pos==0){
+	    LNode current = start;
+	    start=new LNode(value);
+	    start.setNext(current);
+	    end=start;
+	    return true;
+	}else{
+	    LNode current = start;
+	    for (int i=0;i<pos-1;i++){
+		current=current.getNext();
+	    }
+	    LNode n=new LNode(value);
+	    n.setNext(current.getNext());
+	    current.setNext(n);
+	    size++;
+	    if(pos==size-1){
+		end=current.getNext();
+	    }
+	    return true;
+	}
+    }
 
+    public boolean remove(int pos){
+	if(pos>size){
+	    return false;
+	}else if(pos==0){
+	    start=start.getNext();
+	    return true;
+	}else{
+	    LNode current = start;
+	    for (int i=0;i<pos-1;i++){
+		current=current.getNext();
+	    }
+	    current.setNext(current.getNext().getNext());
+	    size--;
+	    return true;
+	}
+    }
+
+    public int get(int pos){
+	if(pos>=size){
+	    System.out.println("Index out of Bounds");
+	}
+	LNode current=start;
+	for (int i=0;i<pos;i++){
+	    current=current.getNext();
+	}
+	return current.getValue();
+    }
+
+    public int set(int value, int pos){
+	if(pos>=size){
+	    System.out.println("Index out of Bounds");
+	}
+	LNode current=start;
+	for (int i=0;i<pos;i++){
+	    current=current.getNext();
+	}
+	int temp=current.getValue();
+	current.setValue(value);
+	return temp;
+    }
+    
+    public int size(){
+	return size;
+    }
+
+    public int indexOf(int value){
+	int pos=0;
+	LNode current=start;
+	while(current.getNext()!=null){
+	    if (current.getValue()==value){
+		return pos;
+	    }
+	    current=current.getNext();
+	}
+	return -1;
+    }
+    
     public String toString(){
 	String ans="[";
 	LNode current = start;
@@ -54,7 +135,17 @@ public class MyLinkedList{
     public static void main(String[]args){
 	MyLinkedList l=new MyLinkedList();
 	l.add(3);
+	System.out.println(l);
+	l.add(-3,1);
+	System.out.println(l);
 	l.add(-3);
 	System.out.println(l);
+	l.remove(1);
+	System.out.println(l);
+	System.out.println(l.get(1));	
+	System.out.println(l.set(3,1));	
+	System.out.println(l);
+	System.out.println(l.indexOf(3));
+	System.out.println(l.indexOf(2));
     }
 }
