2f484b38909d3738d4caa6eeca538cf510fccd5c
gTopa <gtopa@stuy.edu>
2016-04-03 09:06:51 -0400

    done

--- 10MyDeque/MyDeque.java
+++ 10MyDeque/MyDeque.java
@@ -19,7 +19,7 @@ public class MyDeque<T>{
 
     public void addFirst(T value){
 	if (start-1<0){
-	    start=deque.length-1;
+	    start=deque.length;
 	}
 	start--;
 	if(start==end){
@@ -30,7 +30,50 @@ public class MyDeque<T>{
     }
 
     public void addLast(T value){
-	
+	if(end+1>=deque.length){
+	    end=-1;
+	}
+	end++;
+	if (start==end){
+	    grow();
+	}
+	deque[end]=value;
+	size++;
     }
 
+    public T removeFirst(){
+	T temp=(T)deque[start];
+	if(start+1>=deque.length){
+	    start=-1;
+	}
+	start++;
+	size--;
+	return temp;	
+    }
+
+    public T removeLast(){
+	T temp=(T)deque[end];
+	if (end-1<0){
+	    end=deque.length;
+	}
+	end--;
+	size--;
+	return temp;
+    }
+
+    public T getFirst(){
+	return (T)deque[start];
+    }
+
+    public T getLast(){
+	return (T)deque[end];
+    }
+
+    public String toString(){
+	String retr="";
+	for(int i=0;i<deque.length;i++){
+	    retr+=deque[(start+i)%deque.length];
+	}
+	return retr;
+    }
 }
