662300171e24f4782694b6ec61557b4c3f0d0e17
gTopa <gtopa@stuy.edu>
2016-05-07 21:26:59 -0400

    yay

--- 13heap/MyHeap.java
+++ 13heap/MyHeap.java
@@ -7,20 +7,47 @@ public class MyHeap<T extends Comparable<T>>{
     private int size;
     
     public MyHeap(){
-	heap=(T)new Comparable[10];
+	heap=(T[])(new Comparable[10]);
 	isMax=true;
 	size=0;
     }
 
     public MyHeap(boolean max){
-	heap=(T)new Comparable[10];
+	heap=(T[])(new Comparable[10]);
 	isMax=max;
 	size=0;
     }
-
+    
+    public MyHeap(T[] array, boolean max){
+	isMax=max;
+	size=array.length;
+	heap=(T[])new Comparable[array.length+1];
+	for(int i=1;i<=size;i++){
+	    heap[i]=array[i-1];
+	}
+	for(int i=size;i>0;i--){
+	    pushDown(i);
+	}
+    }
+    
+    public MyHeap(T[] array){
+	isMax=true;
+	size=array.length;
+	heap=(T[])new Comparable[array.length+1];
+	for(int i=1;i<=size;i++){
+	    heap[i]=array[i-1];
+	}
+	for(int i=size;i>0;i--){
+	    pushDown(i);
+	}
+    }
+    
     public T remove(){
-	pushDown(1);
+	T temp=heap[1];
+	heap[1]=heap[size];
 	size--;
+	pushDown(1);
+	return temp;
     }
 
     public void add(T value){
@@ -33,7 +60,11 @@ public class MyHeap<T extends Comparable<T>>{
     }
 
     private void resize(){
-
+	T[] temp=(T[])new Comparable[size*2+1];
+	for (int i=1;i<=size;i++){
+	    temp[i]=heap[i];
+	}
+	heap=temp;
     }
 
     private void pushUp(int pos){
@@ -46,20 +77,23 @@ public class MyHeap<T extends Comparable<T>>{
     }
 
     private void pushDown(int pos){
-	if(compare(heap[pos*2],heap[pos*2+1])){
-	    T temp=heap[pos*2];
-	    heap[pos*2]=heap[pos];
-	    heap[pos]=temp;
-	    if (pos*2<=size){
-		pushDown(pos*2);
+	try{
+	    if(heap[pos*2+1]==null||compare(heap[pos*2],heap[pos*2+1])){
+		T temp=heap[pos*2];
+		heap[pos*2]=heap[pos];
+		heap[pos]=temp;
+		if (pos*2<=size){
+		    pushDown(pos*2);
+		}
+	    }else{
+		T temp=heap[pos*2+1];
+		heap[pos*2+1]=heap[pos];
+		heap[pos]=temp;
+		if (pos*2+1<=size){
+		    pushDown(pos*2+1);
+		}
 	    }
-	}else{
-	    T temp=heap[pos*2+1];
-	    heap[pos*2+1]=heap[pos];
-	    heap[pos]=temp;
-	    if (pos*2+1<=size){
-		pushDown(pos*2+1);
-	    }
+	}catch(IndexOutOfBoundsException e){
 	}
     }
 
@@ -74,4 +108,14 @@ public class MyHeap<T extends Comparable<T>>{
 	    return second.compareTo(first)>0;
 	}
     }
+
+    public String toString(){
+	String temp="";
+	for (int i=1;i<=size;i++){
+	    temp+=heap[i]+" ";
+	}
+	return temp;
+	//return Arrays.toString(heap);
+    }
+    
 }
